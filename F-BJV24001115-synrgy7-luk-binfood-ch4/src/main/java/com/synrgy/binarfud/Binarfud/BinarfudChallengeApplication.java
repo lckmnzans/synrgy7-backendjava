ee18d514a75b1f833c7c5a58e856583e4edc7d64
Lukman Sanusi <lckmnzans@gmail.com>
2024-05-02 23:28:53 +0700

    init spring framework

---
+++ F-BJV24001115-synrgy7-luk-binfood-ch4/src/main/java/com/synrgy/binarfud/Binarfud/BinarfudChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.synrgy.binarfud.Binarfud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarfudChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinarfudChallengeApplication.class, args);
+	}
+
+}
