652dd679f851a22e4062268f9f4b95eba4561878
Lukman Sanusi <lckmnzans@gmail.com>
2024-05-17 19:17:06 +0700

    checkpoint 2

--- F-BJV24001115-synrgy7-luk-binfood-ch5/src/main/java/com/synrgy/binarfud/Binarfud/payload/OrderDetailDto.java
+++ F-BJV24001115-synrgy7-luk-binfood-ch5/src/main/java/com/synrgy/binarfud/Binarfud/payload/OrderDetailDto.java
@@ -1,6 +1,7 @@
 package com.synrgy.binarfud.Binarfud.payload;
 
 import com.synrgy.binarfud.Binarfud.model.Order;
+import com.synrgy.binarfud.Binarfud.model.Product;
 import lombok.Data;
 
 import java.util.UUID;
@@ -8,5 +9,12 @@ import java.util.UUID;
 @Data
 public class OrderDetailDto {
     private String id;
+    private String productId;
+    private String productName;
     private int quantity;
+
+    public void setProduct(Product product) {
+        this.productId = product.getId().toString();
+        this.productName = product.getProductName();
+    }
 }
--- F-BJV24001115-synrgy7-luk-binfood-ch5/src/main/java/com/synrgy/binarfud/Binarfud/payload/OrderDto.java
+++ F-BJV24001115-synrgy7-luk-binfood-ch5/src/main/java/com/synrgy/binarfud/Binarfud/payload/OrderDto.java
@@ -4,14 +4,20 @@ import com.synrgy.binarfud.Binarfud.model.Users;
 import lombok.Data;
 
 import java.util.List;
+import java.util.UUID;
 
 @Data
 public class OrderDto {
+    private String orderId;
     private String userName;
     private String destinationAddress;
     private List<OrderDetailDto> orderDetailList;
 
     public void setUser(Users user) {
         this.userName = user.getUsername();
+    }
+
+    public void setId(UUID id) {
+        this.orderId = id.toString();
     }
 }
