98b26b10f26facf75183c968b308f60f03323c70
lckmnzans <lckmnzans@gmail.com>
2024-06-17 13:54:36 +0700

    implement microservices with eureka server

--- F-BJV24001115-synrgy7-luk-binfood-ch7/notification/src/main/java/com/synrgy/binarfud/NotificationApplication.java
+++ F-BJV24001115-synrgy7-luk-binfood-ch7/notification/src/main/java/com/synrgy/binarfud/NotificationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaNotificationApplication {
+public class NotificationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KafkaNotificationApplication.class, args);
+		SpringApplication.run(NotificationApplication.class, args);
 	}
 
 }
