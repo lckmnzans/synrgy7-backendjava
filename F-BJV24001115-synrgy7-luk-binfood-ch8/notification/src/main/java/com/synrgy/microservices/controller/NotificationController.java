0eb365fdbe7556f8c70c51d37898446bffcd3c73
Lukman <lckmnzans@gmail.com>
2024-06-30 14:59:38 +0700

    notification feature done

--- F-BJV24001115-synrgy7-luk-binfood-ch8/notification/src/main/java/com/synrgy/microservices/controller/NotificationController.java
+++ F-BJV24001115-synrgy7-luk-binfood-ch8/notification/src/main/java/com/synrgy/microservices/controller/NotificationController.java
@@ -1,11 +1,25 @@
 package com.synrgy.microservices.controller;
 
+import com.synrgy.microservices.model.NotificationRequest;
+import com.synrgy.microservices.model.NotificationResponse;
+import com.synrgy.microservices.service.FCMService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/fcm")
-public class NotificationController {
+import java.util.concurrent.ExecutionException;
 
+@RestController
+public class NotificationController {
+    @Autowired
+    FCMService fcmService;
+
+    @PostMapping("/notification")
+    public ResponseEntity sendNotification(@RequestBody NotificationRequest request) throws ExecutionException, InterruptedException {
+        fcmService.sendMessageToToken(request);
+        return new ResponseEntity<>(new NotificationResponse(HttpStatus.OK.value(), "Notification has been sent"), HttpStatus.OK);
+    }
 }
